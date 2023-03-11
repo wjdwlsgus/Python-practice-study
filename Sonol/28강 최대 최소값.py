e7ae8c46a6e1ab1441937504e1ed5f97925e0708
wjdwlsgus <mojjh0347@naver.com>
2023-03-11 20:19:35 +0900

    Add files

---
+++ Sonol/28강 최대 최소값.py
@@ -0,0 +1,21 @@
+# 최소값 구하기
+li = list(map(int,input('숫자 입력:').split()))
+
+m = li[0]
+
+for i in li:
+    if i<m:
+        m=i
+
+print(m)
+
+# 최대값 구하기
+li=list(map(int,input('숫자 입력').split()))
+
+m=li[0]
+
+for i in li:
+    if i>m:
+        m=i
+
+print(m)
